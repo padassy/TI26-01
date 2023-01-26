5002c901785f55be54d77ac4846774dcac09985b
padassy <pierre-alain.dassy@cf2000.onmicrosoft.com>
2023-01-26 13:11:48 +0100

    commit pause midi

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 // on importe le fichier config en require once car fichier important et necessaire au site on arrete le script en cas d erreur  
 require_once "config.php";
+$message = " ";
+$messageGood = " ";
 
 // Try catch permettant de recuperer les erreurs en cas de probleme de connexion a la DB
 try {
@@ -22,13 +24,15 @@ try {
 // Creation d une requete qui va recuperer les donnees de la DB
 
 // Variable qui contient la requete SQL 
-$requeteRecupDonneesDB = "SELECT * FROM livreor";
+$requeteRecupDonneesDB = "SELECT `firstname`,`lastname`,`usermail`,`message`,`datemessage` FROM livreor;";
 
 
 // Variable qui envoie la requete SQL a la DB abec la connexion a la DB + la requete
 // On arrete le script en cas d erreur , et affiche le message 
 
 $recupDonneesDB = mysqli_query($donneeDeConnexionSql, $requeteRecupDonneesDB) or die("Erreur de récuperation des données de la DB");
+
+$nbUser = mysqli_num_rows($recupDonneesDB);
 
 
 // On met la resultante dans un tableau associatif 
@@ -41,6 +45,51 @@ $resultatRecupDB = mysqli_fetch_assoc($recupDonneesDB);
 
 // On verifie l'existance des variables Post envoyee par le formulaire avec un isset 
 
-if (isset($_POST['']))
+if (isset($_POST['firstname'], $_POST['lastname'], $_POST['usermail'], $_POST['message'])) {
 
 
+    // on traite les donnees utilisateur avant de les envoyer vers la DB, on enleve les espaces avec le trim , on supprime les tags html avec strip_tags, et on converti les caracteres speciaux en entites html avec html specialchars et on encode les guillemets avec ENT_QUOTES
+    $prenom = htmlspecialchars(strip_tags(trim($_POST['firstname'])), ENT_QUOTES);
+    $nom = htmlspecialchars(strip_tags(trim($_POST['lastname'])), ENT_QUOTES);
+    $mail = htmlspecialchars(strip_tags(trim($_POST['usermail'])), ENT_QUOTES);
+    $texte = htmlspecialchars(strip_tags(trim($_POST['message'])), ENT_QUOTES);
+
+
+
+    // Envoi des donnees traitee precedement 
+// Je verifie que les donnes Post ne sont PAS vides et si elle sont remplie je les stock dans un variable qui contient les instructions SQL vers la DB et je filtre le champs mail qui verifie si c est bien un mail valide 
+    if (!empty($nom) && !empty($texte) && filter_var($mail, FILTER_VALIDATE_EMAIL)):
+        $envoiDesDonneesUtilisateurSql = "INSERT INTO livreor (firstname, lastname, usermail,message) VALUES ('$prenom,'$nom','$mail','$texte')";
+
+        // try catch qui permet la gestion des messages en cas et de reussite de l envoi ou des differentes erreurs 
+
+
+        try {
+
+            // Si la conditon precedente est validee on envoit les données a la DB avec mysqli_query
+            mysqli_query($donneeDeConnexionSql, $envoiDesDonneesUtilisateurSql);
+            // message en cas de reussite de l envoie du formulaire
+            $messageGood = "Merci pour votre commentaire ";
+
+
+        } catch (Exception $e) {
+
+
+            // Ici je vais gérer avec Exception les différentes erreurs si la condition n' est pas verifiee 
+// Si un champs est trop long 
+            if ($e->getCode() == 1406):
+                $message = "Un champs est trop long";
+            endif;
+        }
+
+
+// Si le mail n est pas valide
+    elseif (!filter_var($mail, FILTER_VALIDATE_EMAIL)):
+// Dans tous les autres cas ou ca n a pas envoye le formulaire
+    else:
+        $message = "Il y a eu un problème lors de votre inscription, veuillez réessayer";
+
+    endif;
+}
+
+include "../view/indexView.php";
--- view/indexView.php
+++ view/indexView.php
@@ -1,1 +1,28 @@
+<h2>test vue</h2>
+
+Affichage des erreurs
+<h3><?=$message?></h3>
+<h3><?$messageGood?></h3>
 <?php
+
+
+if (empty($resultatRecupDB)): 
+    ?>
+    
+        <h1>Pas encore d'adresse enregistrée </h1>
+    <?php
+        else:
+    ?>
+        <h2>Adresses présentes dans la database :</h2>
+    <?php
+        foreach ($resultatRecupDB as $item):
+        
+    ?>
+
+        <?php
+        endforeach;
+    ?>
+        <h1>Nombre d'adresses dans la database : <?=$nbUser?></h1>
+    <?php
+    
+    endif;
